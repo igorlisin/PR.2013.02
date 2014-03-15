8f8cdd1d3d0add5f2c4c52d1436fd392b65f69c9
ilshatlatyp <ilshatty@mail.ru>
2014-03-15 11:53:11 +0400

    Класс Object и Objects добавлены в папку Object

--- Program/PR/Classes/Object/Object.cs
+++ Program/PR/Classes/Object/Object.cs
@@ -1,86 +1,205 @@
 using System;
-
+using System.Collections.Generic;
 using PRInterfaces.Interfaces;
+using PRInterfaces.Enumerations.Object;
 
 namespace PR.Classes
 {
+    
+        /// <summary>
+        /// Класс. Объект оценки 
+        /// </summary>
+        public class Object :   Entity, IObject
+        {
+            /// <summary>
+            /// Поле. Тип объекта оценки
+            /// </summary>
+            private string _type;
+            /// <summary>
+            /// Поле. Место
+            /// </summary>
+            //private Place _place;
+            /// <summary>
+            /// Поле. Количество комнат
+            /// </summary>
+            //private int _numberOfRooms;
+            /// <summary>
+            /// Поле. Имущественные права на объект оценки
+            /// </summary>
+            private string _property;
+            /// <summary>
+            /// Поле. Существующие ограничения права
+            /// </summary>
+            private string _restriction;
+            /// <summary>
+            /// Поле. Правообладатели оцениваемого имущества
+            /// </summary>
+            private string _holders;
+            /// <summary>
+            /// Поле. Вид оцениваемой стоимости
+            /// </summary>
+            private string _typeOfValue;
+            /// <summary>
+            /// Поле. Оценочная стоимость
+            /// </summary>
+            private float _price;
+            /// <summary>
+            /// Поле. Ликвидационная уценка
+            /// </summary>
+            private float _discount;
+            /// <summary>
+            /// Поле. Курс доллара
+            /// </summary>
+            private float _dollar;
+            /// <summary>
+            /// Поле. Цель оценки
+            /// </summary>            
+            private string _purposeOfTheEvaluation;
+            /// <summary>
+            /// Поле. Цель оценки
+            /// </summary>            
+            private string _destOfTheEvaluation;
+            /// <summary>
+            /// Свойство. Задает и возвращает тип объекта
+            /// </summary>
+            public string ObjectType
+            {
+                get
+                {
+                    return (_type);
+                }
+                set
+                {
+                    _type = value;
+                }
+            }
 
-namespace Object{
-	/// <summary>
-	/// Класс. Объект оценки
-	/// </summary>
-	private class Object : IObject, Entity{
-		/// <summary>
-		/// Поле. Тип объекта
-		/// </summary>
-		private ObjectTypes _type ;
-		/// <summary>
-		/// Поле. Место
-		/// </summary>
-		private Place _place ;
-		/// <summary>
-		/// Поле. Количество комнат
-		/// </summary>
-		private int _numberOfRooms ;
-		/// <summary>
-		/// Поле. Имущественные права на объект оценки
-		/// </summary>
-		private string _property ;
-		/// <summary>
-		/// Поле. Существующие ограничения права
-		/// </summary>
-		private string _restriction ;
-		/// <summary>
-		/// Поле. Правообладатели оцениваемого имущества
-		/// </summary>
-		private object _holders ;
-		/// <summary>
-		/// Поле. Вид оцениваемой стоимости
-		/// </summary>
-		private object _typeOfValue ;
-		/// <summary>
-		/// Поле. Цель оценки
-		/// </summary>
-		private string _purposeOfTheEvaluation ;
-		/// <summary>
-		/// Свойство. Задает и возвращает тип объекта
-		/// </summary>
-		int IObject.Type(){
-		}
-		/// <summary>
-		/// Свойство. Задает и возвращает место
-		/// </summary>
-		IPlace IObject.Place(){
-		}
-		/// <summary>
-		/// Свойство. Задает и возвращает количество комнат
-		/// </summary>
-		int IObject.NumberOfRooms(){
-		}
-		/// <summary>
-		/// Свойство. Задает и возвращает имущественные права на объект оценки
-		/// </summary>
-		string IObject.Property(){
-		}
-		/// <summary>
-		/// Свойство. Задает и возвращает существующие ограничения права
-		/// </summary>
-		string IObject.Restriction(){
-		}
-		/// <summary>
-		/// Свойство. Задает и возвращает правообладателей оцениваемого имущества
-		/// </summary>
-		List<IMan> IObject.Holders(){
-		}
-		/// <summary>
-		/// Свойство. Задает и возвращает вид оцениваемой стоимости
-		/// </summary>
-		int IObject.TypeOfValue(){
-		}
-		/// <summary>
-		/// Свойство. Задает и возвращает цель оценки
-		/// </summary>
-		string IObject.PurposeOfTheEvaluation(){
-		}
-	}
-}
+            /// <summary>
+            /// Свойство. Задает и возвращает имущественные права на объект оценки
+            /// </summary>
+            public string Property
+            {
+                get
+                {
+                    return (_property);
+                }
+                set
+                {
+                    _property = value;
+                }
+            }
+            /// <summary>
+            /// Свойство. Задает и возвращает существующие ограничения права
+            /// </summary>
+            public string Restriction
+            {
+                get
+                {
+                    return (_restriction);
+                }
+                set
+                {
+                    _restriction = value;
+                }
+            }
+            /// <summary>
+            /// Свойство. Задает и возвращает правообладателей оцениваемого имущества
+            /// </summary>
+            public string Holders
+            {
+                get
+                {
+                    return (_holders);
+                }
+                set
+                {
+                    _holders = value;
+                }
+            }
+            /// <summary>
+            /// Свойство. Задает и возвращает вид оцениваемой стоимости
+            /// </summary>
+            public string TypeOfValue
+            {
+                get
+                {
+                    return (_typeOfValue);
+                }
+                set
+                {
+                    _typeOfValue = value;
+                }
+            }
+            /// <summary>
+            /// Свойство. Задает и возвращает цену
+            /// </summary>
+            public float Price
+            {
+                get
+                {
+                    return (_price);
+                }
+                set
+                {
+                    _price = value;
+                }
+            }
+            /// <summary>
+            /// Свойство. Задает и возвращает уценку
+            /// </summary>
+            public float Discount
+            {
+                get
+                {
+                    return (_discount);
+                }
+                set
+                {
+                    _discount = value;
+                }
+            }
+            /// <summary>
+            /// Свойство. Задает и возвращает курс доллара
+            /// </summary>
+            public float Dollar
+            {
+                get
+                {
+                    return (_dollar);
+                }
+                set
+                {
+                    _dollar = value;
+                }
+            }
+            /// <summary>
+            /// Свойство. Задает и возвращает цель оценки
+            /// </summary>
+            public string PurposeOfTheEvaluation
+            {
+                get
+                {
+                    return (_purposeOfTheEvaluation);
+                }
+                set
+                {
+                    _purposeOfTheEvaluation = value;
+                }
+            }
+            /// <summary>
+            /// Свойство. Задает и возвращает цель оценки
+            /// </summary>
+            public string DestOfTheEvaluation
+            {
+                get
+                {
+                    return (_destOfTheEvaluation);
+                }
+                set
+                {
+                    _destOfTheEvaluation = value;
+                }
+            }
+        }
+    }
+
--- Program/PR/Classes/Object/Objects.cs
+++ Program/PR/Classes/Object/Objects.cs
@@ -1,43 +1,171 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 
 using PRInterfaces.Interfaces;
 
 namespace PR.Classes
 {
+    /// <summary>
+    /// Класс. Список объектов оценки
+    /// </summary>
+    class Objects : IObjects
+    {
+        #region Delegated
 
-namespace Object{
-	/// <summary>
-	/// Класс. Список объектов оценки
-	/// </summary>
-	public class Objects : IObjects{
-		/// <summary>
-		/// Поле. Список объектов оценки
-		/// </summary>
-		private object _objects ;
-		/// <summary>
-		/// Метод. Добавляет объект оценки в список объектов оценки
-		/// </summary>
-		void IObjects.Add(IObject assessObject){
-		}
-		/// <summary>
-		/// Метод. Удаляет объект оценки из списка объектов оценки
-		/// </summary>
-		void IObjects.Remove(IObject assessObject){
-		}
-		/// <summary>
-		/// Метод. Удаляет объект оценки с указанной позицией из списка объектов оценки
-		/// </summary>
-		void IObjects.RemoveByIndex(int index){
-		}
-		/// <summary>
-		/// Свойство. Задает и возвращает список объектов оценки
-		/// </summary>
-		List<IObject> IObjects.Objects(){
-		}
-		/// <summary>
-		/// Метод. Возвращает объект оценки с указанной позицией из списка объектов оценки
-		/// </summary>
-		IObject IObjects.GetObject(int index){
-		}
-	}
-}
+        /// <summary>
+        /// Делегат. Представляет метод сохранения изменений списка 
+        /// </summary>
+        public delegate void SaveChangesDelegate();
+
+        /// <summary>
+        /// Делегат. Представляет метод сохранения изменений списка 
+        /// </summary>
+        private SaveChangesDelegate _saveChangesDelegate;
+
+        #endregion
+
+        #region Fields
+
+        /// <summary>
+        /// Поле. Набор объектов оценки
+        /// </summary>
+        private DbSet<Object> _objectsDbSet;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Свойство. Задает и возвращает набор объектов оценки
+        /// </summary>
+        public DbSet<Object> ObjectsDbSet
+        {
+            get
+            {
+                return (_objectsDbSet);
+            }
+            set
+            {
+                _objectsDbSet = value;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Конструктор объектов оценки
+        /// </summary>
+        public Objects(DbSet<Object> objectsDbSet, SaveChangesDelegate saveChangesDelegate)
+        {
+            _objectsDbSet = objectsDbSet;
+            _saveChangesDelegate = saveChangesDelegate;
+        }
+
+        /// <summary>
+        /// Метод. Создает и возвращает новую объект оценки
+        /// </summary>
+        public IObject Create()
+        {
+            IObject Object;
+
+            Object = (IObject)new Object();
+
+            return (Object);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Метод. Добавляет объект оценки в набор 
+        /// </summary>
+        public void Add(IObject Object)
+        {
+            _objectsDbSet.Add((Object)Object);
+            SaveChanges();
+        }
+
+        /// <summary>
+        /// Метод. Удаляет объект оценки из набора 
+        /// </summary>
+        public void Remove(IObject Object)
+        {
+            _objectsDbSet.Remove((Object)Object);
+            SaveChanges();
+        }
+
+        /// <summary>
+        /// Метод. Удаляет объект оценки с указанным идентификатором из списка 
+        /// </summary>
+        public void RemoveById(int id)
+        {
+            IObject Object;
+
+            Object = GetObject(id);
+
+            if (Object != null)
+            {
+                _objectsDbSet.Remove((Object)Object);
+                SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Метод. Возвращает список 
+        /// </summary>
+        public List<IObject> ToList()
+        {
+            return (_objectsDbSet.ToList<IObject>());
+        }
+
+        /// <summary>
+        /// Метод. Возвращает массив 
+        /// </summary>
+        public IObject[] ToArray()
+        {
+            return (_objectsDbSet.ToArray<IObject>());
+        }
+
+        /// <summary>
+        /// Метод. Возвращает объект оценки с указанным идентификатором из списка 
+        /// </summary>
+        public IObject GetObject(int id)
+        {
+            return ((IObject)_objectsDbSet.Where(a => a.Id == id).First());
+        }
+
+        /// <summary>
+        /// Метод. Возвращает количество элементов в списке
+        /// </summary>
+        public int Count()
+        {
+            return (_objectsDbSet.Count());
+        }
+
+        /// <summary>
+        /// Метод. Возвращает перечислитель
+        /// </summary>
+        public IEnumerator GetEnumerator()
+        {
+            return (new EntityEnumerator(_objectsDbSet.ToArray<Object>()));
+        }
+
+        /// <summary>
+        /// Метод. Сохраняет изменения списка 
+        /// </summary>
+        public void SaveChanges()
+        {
+            _saveChangesDelegate();
+        }
+
+  
+        #endregion
+    }
+    }
+
